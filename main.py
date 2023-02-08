1c63dc5f54a11ef5edc30f20d42b6ea645799f02
cooldudedev29 <120023660+cooldudedev29@users.noreply.github.com>
2023-02-08 11:54:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import time
+import pygame
+time.sleep(1)
+print("made in python")                      
+time.sleep(2)  
+print("")
+print("hello i am al,thank you for particpating in the ai survey test")
+time.sleep(2)
+print("")
+print("i am sorry i did not get your name,")
+print("")
+print("what was your name?")
+name=input('')
+print("hi "+(name))
